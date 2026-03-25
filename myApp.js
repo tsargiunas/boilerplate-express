397e9bcb440a1b92dafe6bd38acc64f3e30f0c0c
Titas Sargiunas <tsargiunas@mail.lipscomb.edu>
2026-03-24 22:08:11 -0500

    Enable static file serving and set up server port
    Added static file serving and configured server port.

--- myApp.js
+++ myApp.js
@@ -3,6 +3,13 @@ let app = express();
 
 console.log("Hello World");
 
+app.use(express.static(__dirname + '/public'));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT);
+
+module.exports = app;
+
 
 
 
